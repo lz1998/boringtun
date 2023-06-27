b4edeaaed08e13cd1a9b2a47cc2f74614b9596a8
lz1998 <875543533@qq.com>
2023-06-27 13:21:23 +0800

    update

--- boringtun/src/device/binary.rs
+++ boringtun/src/device/binary.rs
@@ -4,6 +4,7 @@ pub trait BinaryReader: Read {
     fn read_u32(&mut self) -> Result<u32> {
         let mut buf = [0u8; 4];
         self.read_exact(&mut buf)?;
+        println!("read u32: {}", hex::encode(buf));
         Ok(u32::from_be_bytes(buf))
     }
 
@@ -16,12 +17,14 @@ pub trait BinaryReader: Read {
         let len = self.read_u32()? as usize;
         let buf = &mut buf[..len];
         self.read_exact(buf)?;
+        println!("read buf: {}", hex::encode(&buf));
         Ok(buf)
     }
 }
 
 pub trait BinaryWriter: Write {
     fn write_u32(&mut self, n: u32) -> Result<()> {
+        println!("write u32: {}", hex::encode(n.to_be_bytes()));
         self.write_all(&n.to_be_bytes())
     }
 
@@ -31,9 +34,29 @@ pub trait BinaryWriter: Write {
 
     fn write_packet(&mut self, buf: &[u8]) -> Result<()> {
         self.write_u32(buf.len() as u32)?;
+        println!("write buf: {}", hex::encode(&buf));
         self.write_all(buf)
     }
 }
 
-impl BinaryReader for socket2::Socket {}
-impl BinaryWriter for socket2::Socket {}
+impl BinaryReader for socket2::Socket {
+    fn read_packet<'a>(&mut self, buf: &'a mut [u8]) -> Result<&'a mut [u8]> {
+        self.set_nonblocking(false).unwrap();
+        let len = self.read_u32()? as usize;
+        let buf = &mut buf[..len];
+        self.read_exact(buf)?;
+        println!("read buf: {}", hex::encode(&buf));
+        self.set_nonblocking(true).unwrap();
+        Ok(buf)
+    }
+}
+impl BinaryWriter for socket2::Socket {
+    fn write_packet(&mut self, buf: &[u8]) -> Result<()> {
+        self.set_nonblocking(false).unwrap();
+        self.write_u32(buf.len() as u32)?;
+        println!("write buf: {}", hex::encode(&buf));
+        self.write_all(buf)?;
+        self.set_nonblocking(true).unwrap();
+        Ok(())
+    }
+}
